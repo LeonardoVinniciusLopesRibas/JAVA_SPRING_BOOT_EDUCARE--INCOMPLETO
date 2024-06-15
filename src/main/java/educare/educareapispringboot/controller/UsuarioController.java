a95046a610c9d3f381e3fbc1119b4fd79d776f72
LeonardoVinniciusLopesRibas <110777424+LeonardoVinniciusLopesRibas@users.noreply.github.com>
2024-06-15 17:45:25 -0300

    Novo commit

--- src/main/java/educare/educareapispringboot/controller/UsuarioController.java
+++ src/main/java/educare/educareapispringboot/controller/UsuarioController.java
@@ -2,10 +2,13 @@ package educare.educareapispringboot.controller;
 
 import educare.educareapispringboot.dto.UsuarioRequestValidation;
 import educare.educareapispringboot.service.UsuarioService;
-import jakarta.validation.Valid;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -15,10 +18,47 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @PostMapping("/validacao")
-    public ResponseEntity<Boolean> validaUsuario(@RequestBody @Valid UsuarioRequestValidation urv){
-        return ResponseEntity.ok(usuarioService.validaUsuario(urv));
+    private Map<String, HttpSession> sessionMap = new HashMap<>();
+
+    @PostMapping("/login")
+    public ResponseEntity<Boolean> login(HttpSession session, @RequestBody UsuarioRequestValidation urv) {
+        Boolean isAuthenticated = usuarioService.validaUsuario(urv);
+
+        if (isAuthenticated) {
+            session.setAttribute("user", urv.getEmail());
+            sessionMap.put(session.getId(), session);
+        }
+
+        return ResponseEntity.ok(isAuthenticated);
     }
 
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(HttpSession session) {
+        // Remover a sessão do usuário ao fazer logout
+        session.invalidate();
+        sessionMap.remove(session.getId());
+        return ResponseEntity.noContent().build();
+    }
 
+    @GetMapping("/session")
+    public ResponseEntity<Boolean> checkSession(HttpSession session) {
+        // Verificar se o usuário possui uma sessão válida
+        Boolean isLoggedIn = session.getAttribute("user") != null;
+        return ResponseEntity.ok(isLoggedIn);
+    }
+
+    // Endpoint para validar se o usuário tem permissão para acessar uma página protegida
+    @GetMapping("/valida-permissao")
+    public ResponseEntity<Boolean> validaPermissao(HttpSession session) {
+        Boolean isLoggedIn = session.getAttribute("user") != null;
+        return ResponseEntity.ok(isLoggedIn);
+    }
+
+    // Endpoint para encerrar todas as sessões ativas (opcional)
+    @GetMapping("/encerrar-sessoes")
+    public ResponseEntity<Void> encerrarTodasSessoes() {
+        sessionMap.values().forEach(HttpSession::invalidate);
+        sessionMap.clear();
+        return ResponseEntity.noContent().build();
+    }
 }
