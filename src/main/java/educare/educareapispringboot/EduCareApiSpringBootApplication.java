7abd8cb074e8459c9dd9c3caeee4d5f93bcd8b2c
LeonardoVinniciusLopesRibas <110777424+LeonardoVinniciusLopesRibas@users.noreply.github.com>
2024-06-15 17:36:07 -0300

    Novo commit
    Ajustado liberação para uso no navegador

--- src/main/java/educare/educareapispringboot/EduCareApiSpringBootApplication.java
+++ src/main/java/educare/educareapispringboot/EduCareApiSpringBootApplication.java
@@ -2,12 +2,20 @@ package educare.educareapispringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EduCareApiSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EduCareApiSpringBootApplication.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(4848);
     }
 
 }
